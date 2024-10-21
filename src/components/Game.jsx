dfb5d8304447c6bc28f60c6cdc90b8cb82b9bb78
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-10-21 19:34:45 +0200

    Ora i dati vengono presi e il render delle card  parte subito dopo.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,58 +1,51 @@
 import { useState, useEffect, useRef } from 'react';
 
-const ids = [1, 2, 3];
-let hasLoaded = false;
-
 function Card({ url, desc }) {
     return (
-       <>
         <div className="card">
             <img src={url} alt="" />
             <p>{desc}</p>
         </div>
-       </> 
     );
 }
 
 export default function Game() {
+    const [ids, setIds] = useState([1, 2, 3]);
     const [data, setData] = useState([]);
-    const renderCount = useRef(0);
-
-    console.log('Render number: ', renderCount.current);
-    renderCount.current += 1;
-    console.log(data);
+    const loading = useRef(true);
 
     async function getImages(list) {
-        const fetchedData = [];
-        list.map((id) => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-                .then(response => response.json())
-                .then(json => {
-                    fetchedData.push([id, json.name, json.sprites.front_default]);
-                }).catch(error => {
-                    console.error(error);
-                    fetchedData.push([id, '', '']);
-                });
-            });
-        setData(fetchedData);
+        loading.current = true;
+        try {
+            const fetchedData = await Promise.all(
+                list.map(async (id) => {
+                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+                    const json = await response.json();
+                    return [id, json.name, json.sprites.front_default];
+                })
+            );
+            setData(fetchedData);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            loading.current = false;
+        }
     };
 
     useEffect(() => {
-        console.log('has loaded: ', hasLoaded);
-        if (!hasLoaded) {
-            hasLoaded = true;
-            getImages(ids);
-        } else {
-            console.log('not running');
-        };
-    }, []);
+        getImages(ids);
+    }, [ids]);
 
     return (
         <div className="board">
-            {!hasLoaded ? <div>Loading</div> :
-            data.map((pokemon) => {
-                return <Card key={pokemon.id} url={pokemon[2]} desc={pokemon[1]} />
-            })}
+            
+            {loading.current && <div>Loading</div>}
+            {
+                !loading.current &&
+                data.map((pokemon) => {
+                    return <Card key={pokemon[0]} url={pokemon[2]} desc={pokemon[1]} />;
+                })
+            }
         </div>
-    )
+    );
 }
